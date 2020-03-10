01d4266e729f22fe26ac7936637df4c2c9a9ee3f
thomashaile <gthomashaile@gmail.com>
2020-03-10 22:21:42 +0100

    test

--- II.write-tests/09-test-async/async-1.test.js
+++ II.write-tests/09-test-async/async-1.test.js
@@ -3,6 +3,7 @@ const getRepos = require("./async-1");
 test("gets a list of repositories names", function() {
   // arrange
   var url = "https://api.github.com/users/kabaros/repos";
+  
   // act
   return getRepos(url).then(function(result) {
     // assert
