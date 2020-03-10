26db56c6a381ac2f86cb2e19c2aaf04f8501ddf4
thomashaile <gthomashaile@gmail.com>
2020-03-10 22:15:26 +0100

    tests

--- II.write-tests/07-get-even-numbers/get-even-numbers.test.js
+++ II.write-tests/07-get-even-numbers/get-even-numbers.test.js
@@ -1,3 +1,20 @@
+var getEven = require("./get-even-numbers");
+test("get even numbers", function() {
+  // Arrange
+  var input = [22, 13, 73, 82, 4];
+  var unchanged = [22, 13, 73, 82, 4];
+  var expected = [22, 82, 4];
+  // Act
+  var result = getEven(input);
+  // Assert
+  expect(result).toEqual(expected);
+  expect(unchanged).toEqual(input);
+});
+
 // example
 // input: [22, 13, 73, 82, 4];
 // expected: [22, 82, 4];
+
+/*
+ test code passed
+*/
